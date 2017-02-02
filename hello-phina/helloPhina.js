fb2cc2cd3c7bead6394b5c17bae4b372c04173f7
KouIshihara <as.it.were.kou@gmail.com>
2017-02-02 14:24:20 +0900

    みずたま

--- hello-phina/helloPhina.js
+++ hello-phina/helloPhina.js
@@ -1,65 +1,85 @@
 phina.globalize();
 
-phina.define('Circle', {
-  superClass: 'CircleShape',
+var addTween = function(shape){
+  /*
+  shape.tweener
+  .to({
+    rotation:360,
+    scaleX:2.0,
+    scaleY:2.0,
+  },1000,"swing")
+  .to({
+    rotation:720,
+  },1000,"swing")
+  .to({
+    rotation:360,
+    scaleX:0.0,
+    scaleY:0.0,
+  },1000,"swing")
+  .call(function(){
+    shape.remove();
+  });
+  */
 
-  init: function(options) {
-    options = (options || {}).$safe({
-      fill: '#FFF',  // 塗りつぶし色
-      stroke: null, // ストローク色
-      radius: 1, // 半径
-    });
-    this.superInit(options);
-    this.blendMode = 'lighter';
-  },
+  shape.tweener
+  .to({
+    alpha:0,
+    scaleX:8.0,
+    scaleY:8.0,
+  },2000,"swing")
+  .call(function(){
+    shape.remove();
+  });
+}
 
-  update: function() {
+var circles = []; // 動的配列的に扱うよ
+var createCircle = function(){
+  var i = circles.length;
+  circles[i] = CircleShape().addChildTo(scene);
+  var randomPosX = Math.random()*scene.gridX.width;
+  var randomPosY = Math.random()*scene.gridY.width;
+  circles[i].x = randomPosX;
+  circles[i].y = randomPosY;
+  circles[i].fill = false;
+  circles[i].radius = Math.random()*20+10;
+  circles[i].stroke = '#AAF';
+  circles[i].strokeWidth = 0.4;
 
-  },
-});
+  addTween(circles[i]);
+}
 
+var createShape = function(){
+  createCircle();
+  createCircle();
+  createCircle();
+}
+
+var scene;
 phina.define('MainScene', {
   superClass: 'CanvasScene',
-  
   init: function init() {
     this.superInit();
-
+    scene = this;
     // 背景色
-    this.backgroundColor = '#FF0';
+    this.backgroundColor = '#FFF';
 
-    var circle = CircleShape().addChildTo(this);
-    circle.x = 200; // x 座標を指定
-    circle.y = 480; // y 座標を指定
-    
-    (200).times(function() {
-      var randomX = Math.random()*window.innerWidth;
-      var randomY = Math.random()*window.innerHeight;
-      this.addCircle(randomX, randomY, 20);      
-    }, this);
-
+    setInterval(createShape , 400);
   },// init
 
   update: function() {
-
+    //app.canvas.canvas.width += 10;
+    //console.log(1);
   },//update
-
-  // circleを追加する関数
-  addCircle: function(x, y, radius) {
-    var color = '#4FF';
-    // サークルを生成
-    var circle = Circle({
-      fill: color,
-      x: x,
-      y: y,
-      radius: radius,
-    }).addChildTo(this);
-  },// addCircle
 });
 
+var app;
 phina.main(function() {
-  var app = GameApp({
+  app = GameApp({
     startLabel: 'main',
   });
+  /*winW = window.innerWidth;
+  var winH = window.innerHeight;
+  app.canvas.canvas.width = winW;
+  app.canvas.canvas.height = winH;*/
   app.run();
 });
-
