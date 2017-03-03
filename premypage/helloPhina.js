c086c7678165c6f8baf1d66578ecb3bb474597d2
KouIshihara <as.it.were.kou@gmail.com>
2017-03-03 15:09:25 +0900

    保存

--- premypage/helloPhina.js
+++ premypage/helloPhina.js
@@ -1,29 +1,32 @@
 phina.globalize();
 
 phina.define('VisualizeRect', {
-  superClass: 'RectangleShape',
+  superClass: 'Shape',
 
   init: function(options) {
     // 引数が指定されなかった時のデフォルト値
     options = (options || {}).$safe({
-      fill: '#999',
-      scaleX: 1.0,
-      scaleY: 1.0,
+      backgroundColor: '#999',
+      scaleX: 0.02,
+      scaleY: 0.05,
     });
     this.superInit(options);
     // 基準点の変更
     this.setOrigin(0.0, 0.5);
 
     this.index = options.index;
-    this.firstPos = {x: this.x, y: this.y};
-    this.firstSize = {width: this.width, height: this.height};
     this.radian = 0;
   },
 
   update: function(app) {
   
     if(GLOBAL.spectrumsArray[this.index] && app.frame % 4 === 0){
-      this.width = 4+GLOBAL.spectrumsArray[this.index]/3;
+      //this.width = 4+GLOBAL.spectrumsArray[this.index]/3;
+      this.scaleX = 0.02 + GLOBAL.spectrumsArray[this.index]/255;
+
+      if(GLOBAL.spectrumsArray[this.index]==0){
+        this.scaleX = 0.02;
+      }
     }
   
     /*
@@ -43,6 +46,7 @@ phina.define('MainScene', {
 
     this.createVisualiza(this.gridX.width/2, this.gridY.width/2);
 
+    
   },
 
   update: function(app) {
@@ -70,7 +74,7 @@ phina.define('MainScene', {
     for(var i=-16; i<48; i++){
       var visRecX = Math.cos(i*5.625 * (Math.PI / 180))*radius;
       var visRecY = Math.sin(i*5.625 * (Math.PI / 180))*radius;
-      this.addVisualizeRect(x+visRecX, y+visRecY, 0.4, 0.4, i*5.625, i+16);
+      this.addVisualizeRect(x+visRecX, y+visRecY, 80, 20, i*5.625, i+16);
       //--------------------x座標, y, width, height, rotation, index番号
     }
     
@@ -79,7 +83,7 @@ phina.define('MainScene', {
     for(var i=-8; i<24; i++){
       var visRecX = Math.cos(i*11.25 * (Math.PI / 180))*radius;
       var visRecY = Math.sin(i*11.25 * (Math.PI / 180))*radius;
-      this.addVisualizeRect(x+visRecX, y+visRecY, 2, 2, i*11.25, i+8);
+      this.addVisualizeRect(x+visRecX, y+visRecY, 4, 4, i*11.25, i+8);
       //--------------------x座標, y, width, height, rotation, index番号
     }
     */
--- premypage2/helloPhina.js
+++ premypage2/helloPhina.js
@@ -1,11 +1,7 @@
 phina.globalize();
 
-// アニメーションのステータス
-var STATUS = {
-  normal: true,
-  separation: false,
-  bluefocus: false, 
-}
+var SCREEN_WIDTH;
+var SCREEN_HEIGHT;
 
 phina.define('Circle', {
   superClass: 'CircleShape',
@@ -13,7 +9,6 @@ phina.define('Circle', {
   init: function(options) {
     // 引数がわたされなかった場合のプロパティ
     options = (options || {}).$safe({
-      fill: '#00F',
       stroke: null,
       radius: 10,
       scaleX: 1.0,
@@ -21,56 +16,76 @@ phina.define('Circle', {
     });
     this.superInit(options);
     //this.blendMode = 'lighter';
+    this.alpha = 0.4;
+    this.status = 'normal';
     // STATUS.normalで使うプロパティ---------------
     var speed = Math.random()*2+0.4;
     var angle = Math.randint(0, 360);
     this.vx = Math.cos(angle * (Math.PI / 180)) * speed;
     this.vy = Math.sin(angle * (Math.PI / 180)) * speed;
 
-    this.limitRadius = 200; // 移動できる幅の上限
-    this.moveDistance = 100; // 移動した長さ
+    this.limitRadius = 200; // 移動できる幅の上限 小さい玉ほど端っこまで移動した方が綺麗かも（アイディア）
+    this.moveDistance = this.limitRadius/2; // 移動した長さ
+    // this.accelとかつけてピアノの響きで加速したりするといいかも（アイディア）
     //-------------------------------------------
-    this.unnormal = true;
+    this.tweening = false;
   },//init
 
   update: function() {
-    switch(true){
-      case STATUS.normal:
-        this.normal();
+    switch(this.status){
+      case 'normal':
+        this.normalUpdate();
       break;
-
-      case STATUS.separation:
-        this.separate();
+      case 'separation':
+        this.separateUpdate();
       break;
     } 
   },// update
 
-  normal: function(){
+  normalUpdate: function(){
     this.x += this.vx;
     this.y += this.vy;
 
     this.moveDistance += Math.sqrt(this.vx*this.vx + this.vy*this.vy);
-    if(this.moveDistance > 200){
+    if(this.moveDistance > this.limitRadius){
       this.moveDistance = 0;
       this.vx = -this.vx;
       this.vy = -this.vy;
     }
   },// normal
 
-  separate: function(){
-    if(this.unnormal){
+  separateUpdate: function(){
+    // 一度だけ呼び出す。
+    if(!(this.tweening)){
+
+      // 移動先を決定する。
+      var toPos;
+      switch(this.fill){
+        case '#F00':
+          toPos = {x:SCREEN_WIDTH/2, y:SCREEN_HEIGHT/3};
+        break;
+        case '#0F0':
+          toPos = {x:SCREEN_WIDTH/4, y:SCREEN_HEIGHT*2/3};
+        break;
+        case '#00F':
+          toPos = {x:SCREEN_WIDTH*3/4, y:SCREEN_HEIGHT*2/3};
+        break;
+      } 
+
+      var self = this;
       var durationTime = Math.randint(800, 1600)
       this.tweener
       .to({
-        x:100,
-        y:100,
+        x: toPos.x,
+        y: toPos.y,
       },durationTime,"easwInCubic")
       .call(function(){
-        STATUS.normal = true;
-        STATUS.separate = false;
+        self.status = 'normal';
+        self.tweening = false;
+        self.limitRadius = 80;
       });
-      this.unnormal = false;
-    }
+      this.tweening = true;
+    } // if
   },// separate
 });
 
@@ -79,15 +94,30 @@ phina.define('MainScene', {
   superClass: 'DisplayScene',
   init: function init(options) {
     this.superInit(options);
+
+    SCREEN_WIDTH = this.gridX.width;
+    SCREEN_HEIGHT = this.gridY.width;
+
     // 背景色
     this.backgroundColor = '#FFF';
 
     (30).times(function() {
-    // サークルを生成
+      var random = Math.randint(1,3);
+      var fill;
+      if(random==1){
+        fill='#F00';
+      }else if(random==2){
+        fill='#0F0';
+      }else{
+        fill='#00F';
+      }
+      var radius = Math.random()*20+8;
+      // サークルを生成
       var circle = Circle({
         x: this.gridX.width/2,
         y: this.gridY.width/2,
-        radius: 10,
+        radius: radius,
+        fill: fill,
       }).addChildTo(this);
     }, this);
 
@@ -95,12 +125,11 @@ phina.define('MainScene', {
   },
 
   update: function(app) {
-    if (app.frame % 4 === 0) {
-      
-    }
-    if (app.frame == 100) {
-      STATUS.normal=false;
-      STATUS.separation=true;
+    if (app.frame == 200) {
+      var children = this.children;
+      for(var i=0; i<children.length; i++){
+        children[i].status = 'separation';
+      }
     }
   },//update
 
@@ -135,6 +164,8 @@ phina.main(function() {
     app.canvas.height = winH;
     app.canvas.canvas.width = winW;
     app.canvas.canvas.height = winH;
+    SCREEN_WIDTH = winW;
+    SCREEN_HEIGHT = winH;
   }
 });
 
